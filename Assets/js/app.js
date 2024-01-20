a7d777219d653d320ce8ec70157f191fc5c82af1
Onome <preciousanne2002@gmail.com>
2024-01-20 18:57:38 +0100

    modified

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -44,3 +44,12 @@ function gridView() {
     elements[i].style.width = "50%";
   }
 }
+
+
+$(document).ready(function () {
+    $('.home-header-slide-container').slidejs({
+      start: 1, // Start with the first slide
+      time: 5000, // Time between slides in milliseconds (adjust as needed)
+      auto: true, // Auto play the carousel
+    });
+  });
